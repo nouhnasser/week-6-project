02d2aeffeebc9b519a6c378a5aec07be66e8114a
nouh <nuuhnasir12@gmail.com>
2023-07-18 18:31:04 +0400

    u

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Content = () => {
  
