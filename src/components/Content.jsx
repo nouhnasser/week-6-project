c3cebc7fd660fccdf945e1962bec7c58dcad1014
nouh <nuuhnasir12@gmail.com>
2023-07-19 00:28:31 +0400

    u

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,16 +2,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Ring } from "@uiball/loaders";
+
+<Ring size={40} lineWeight={5} speed={2} color="black" />;
 
 const Content = () => {
- 
   const [showMovie, setShowMovie] = useState([]);
   const [search, setSearch] = useState("");
+  const [load, setLoad] = useState(false);
 
   function submit(event) {
     if (event.key === "Enter") {
-      event.preventDefault();
+     
       fetchMovies();
+      setLoad(true)
     }
   }
 
@@ -19,33 +23,36 @@ const Content = () => {
     const { data } = await axios.get(
       `https://www.omdbapi.com/?apikey=d90f3a14&s=${search}`
     );
-    setShowMovie(data.Search || []);
+    setShowMovie(data.Search || []);  
+    setLoad(false)
   }
 
   useEffect(() => {
-    fetchMovies();
+    setTimeout(() => {
+      fetchMovies();
+    }, 3000);
   }, []);
   return (
     <>
       <body>
         <section id="header">
-        <div className="row">
-          <nav>
-            <h1 className="logo">Imovie</h1>
-            <ul className="nav__list">
-              <li className="nav__links">
-                <Link to="/" className="nav__link">
-                  Home
-                </Link>
-              </li>
-              <li className="nav__links">
-                <Link to="/" className="nav__link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          
+          <div className="row">
+            <nav>
+              <h1 className="logo">Imovie</h1>
+              <ul className="nav__list">
+                <li className="nav__links">
+                  <Link to="/" className="nav__link">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav__links">
+                  <Link to="/" className="nav__link">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+
             <div className="search__box">
               <h3>Search for movies</h3>
               <div className="search__bar">
@@ -70,25 +77,31 @@ const Content = () => {
                 <h2 className="section__title">Featured Movies</h2>
               </div>
               <div className="movies">
-                {showMovie
-                  .map((movie) => (
-                    <div className="movie" key={movie.imdbID}>
-                      <figure className="movie__img--wrapper">
-                        <Link to={`/izk/${movie.imdbID}`}>
-                          <img
-                            src={movie.Poster}
-                            alt={movie.Title}
-                            className="movie__img"
-                          />
-                        </Link>
-                      </figure>
-                      <div className="movie__description">
-                        <p className="movie__name">{movie.Title}</p>
-                        <p className="movie__date">{movie.Year}</p>
+                {load ? (
+                  <svg class="ring" viewBox="25 25 50 50" stroke-width="5">
+                    <circle cx="50" cy="50" r="20" />
+                  </svg>
+                ) : (
+                  showMovie
+                    .map((movie) => (
+                      <div className="movie" key={movie.imdbID}>
+                        <figure className="movie__img--wrapper">
+                          <Link to={`/izk/${movie.imdbID}`}>
+                            <img
+                              src={movie.Poster}
+                              alt={movie.Title}
+                              className="movie__img"
+                            />
+                          </Link>
+                        </figure>
+                        <div className="movie__description">
+                          <p className="movie__name">{movie.Title}</p>
+                          <p className="movie__date">{movie.Year}</p>
+                        </div>
                       </div>
-                    </div>
-                  ))
-                  .slice(0, 6)}
+                    ))
+                    .slice(0, 6)
+                )}
               </div>
             </div>
           </div>
--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,41 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import { Waveform } from '@uiball/loaders'
+
+<Waveform 
+ size={40}
+ lineWeight={3.5}
+ speed={1} 
+ color="black" 
+/>
 
 const MovieInfo = () => {
-  const {imdbID} = useParams()
-  const [movieInfo, setMovieInfo] = useState([])
+  const { imdbID } = useParams();
+  const [movieInfo, setMovieInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   async function movieFetch() {
-    const { data } = await axios.get(`https://www.omdbapi.com/?apikey=d90f3a14&i=${imdbID}`)
-    setMovieInfo(data || [])
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?apikey=d90f3a14&i=${imdbID}`
+    );
+    setMovieInfo(data || []);
+    setLoading(false);
   }
 
-  useEffect(() => { 
-    movieFetch()
-  },[imdbID])
-  
+  useEffect(() => {
+    setTimeout(() => {
+      movieFetch();
+    }, 1000);
+  }, [imdbID]);
+
   return (
     <>
-      <div className="movies__body">
-        <div className="row">
-          <div className="movieinfo__link">
-            <Link to="/">
-             <h2 className="movieinfo__link--home">Click to home</h2>
-            </Link>
-          </div>
-          <div className="movieinfo__wrapper">
-            <div className="movieinfo__img--wrapper">
-             <img src={movieInfo.Poster} alt="" className="movieinfo__img" />
+      {loading ? (
+        <>
+        <div className="movies__body">
+          <div className="row">
+
+         
+        <div class="waveform">
+  <div class="waveform__bar"></div>
+  <div class="waveform__bar"></div>
+  <div class="waveform__bar"></div>
+  <div class="waveform__bar"></div>
+</div>
+</div>
+        </div>
+   
+        </>
+      ) : (
+        <div className="movies__body">
+          <div className="row">
+            <div className="movieinfo__link">
+              <Link to="/">
+                <h2 className="movieinfo__link--home">Click to home</h2>
+              </Link>
             </div>
-            <div className="movieinfo__des--wrapper">
-              <div className="movieinfo__title">Name: {movieInfo.Title}</div>
-              <div className="movieinfo__date">Date: {movieInfo.Year}</div>
-              <div className="movieinfo__summary">Summary: {movieInfo.Plot}</div>
+            <div className="movieinfo__wrapper">
+              <div className="movieinfo__img--wrapper">
+                <img src={movieInfo.Poster} alt="" className="movieinfo__img skeleton" />
+              </div>
+              <div className="movieinfo__des--wrapper">
+                <div className="movieinfo__title skeleton">Name: {movieInfo.Title}</div>
+                <div className="movieinfo__date skeleton">Date: {movieInfo.Year}</div>
+                <div className="movieinfo__summary skeleton">
+                  Summary: {movieInfo.Plot}
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
