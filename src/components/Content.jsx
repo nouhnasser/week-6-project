759b71d5c4343d33c7b9e24e0dd8c95b98d40d03
nouh <nuuhnasir12@gmail.com>
2023-07-19 18:22:01 +0400

    u

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -11,11 +11,21 @@ const Content = () => {
   const [search, setSearch] = useState("");
   const [load, setLoad] = useState(false);
 
+  function filterMovies(filter) {
+    if (filter === "OLDEST") {
+      setShowMovie(showMovie.slice().sort((a, b) => a.Year - b.Year));
+    }
+    if (filter === "NEWEST") {
+      setShowMovie(showMovie.slice().sort((a, b) => b.Year - a.Year));
+    }
+  }
+
   function submit(event) {
     if (event.key === "Enter") {
-     
-      fetchMovies();
-      setLoad(true)
+      setTimeout(() => {
+        fetchMovies();
+      }, 1000);
+      setLoad(true);
     }
   }
 
@@ -23,90 +33,102 @@ const Content = () => {
     const { data } = await axios.get(
       `https://www.omdbapi.com/?apikey=d90f3a14&s=${search}`
     );
-    setShowMovie(data.Search || []);  
-    setLoad(false)
+    setShowMovie(data.Search || []);
+    setLoad(false);
   }
 
   useEffect(() => {
-    setTimeout(() => {
-      fetchMovies();
-    }, 3000);
+    fetchMovies();
   }, []);
   return (
     <>
-      <body>
-        <section id="header">
-          <div className="row">
-            <nav>
-              <h1 className="logo">Imovie</h1>
-              <ul className="nav__list">
-                <li className="nav__links">
-                  <Link to="/" className="nav__link">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav__links">
-                  <Link to="/" className="nav__link">
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+      <section id="header">
+        <div className="row">
+          <nav>
+            <h1 className="logo">Imovie</h1>
+            <ul className="nav__list">
+              <li className="nav__links">
+                <Link to="/" className="nav__link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav__links">
+                <Link to="/" className="nav__link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-            <div className="search__box">
-              <h3>Search for movies</h3>
-              <div className="search__bar">
-                <input
-                  type="text"
-                  placeholder="Search your movie"
-                  className="search"
-                  onChange={(e) => setSearch(e.target.value)}
-                  onKeyDown={submit}
-                />
-                <div className="searchmovies" onClick={fetchMovies}>
-                  <FontAwesomeIcon icon="magnifying-glass" />
-                </div>
+          <div className="search__box">
+            <h3>Search for movies</h3>
+            <div className="search__bar">
+              <input
+                type="text"
+                placeholder="Search your movie"
+                className="search"
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={submit}
+              />
+              <div className="searchmovies" onClick={fetchMovies}>
+                <FontAwesomeIcon icon="magnifying-glass" />
               </div>
             </div>
           </div>
-        </section>
-        <section id="content">
-          <div className="row">
-            <div className="container">
+        </div>
+      </section>
+      <section id="content">
+        <div className="row">
+          <div className="container">
+            {showMovie && 
               <div className="section__description">
-                <h2 className="section__title">Featured Movies</h2>
+                <h2 className="section__title ">Search results : </h2>
+                <select
+                  id="filter"
+                  defaultValue="DEFAULT"
+                  onChange={(event) => filterMovies(event.target.value)}
+                >
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="OLDEST">Date, Oldest</option>
+                  <option value="NEWEST">Date, Newest</option>
+                </select>
               </div>
-              <div className="movies">
-                {load ? (
-                  <svg class="ring" viewBox="25 25 50 50" stroke-width="5">
+            }
+
+            <div className="movies">
+              {load ? (
+                <>
+                  <svg className="ring" viewBox="25 25 50 50" stroke-width="5">
                     <circle cx="50" cy="50" r="20" />
                   </svg>
-                ) : (
-                  showMovie
-                    .map((movie) => (
-                      <div className="movie" key={movie.imdbID}>
-                        <figure className="movie__img--wrapper">
-                          <Link to={`/izk/${movie.imdbID}`}>
-                            <img
-                              src={movie.Poster}
-                              alt={movie.Title}
-                              className="movie__img"
-                            />
-                          </Link>
-                        </figure>
-                        <div className="movie__description">
-                          <p className="movie__name">{movie.Title}</p>
-                          <p className="movie__date">{movie.Year}</p>
-                        </div>
+                </>
+              ) : (
+                showMovie
+                  .map((movie) => (
+                    <div className="movie" key={movie.imdbID}>
+                      <figure className="movie__img--wrapper">
+                        <Link to={`/izk/${movie.imdbID}`}>
+                          <img
+                            src={movie.Poster}
+                            alt={movie.Title}
+                            className="movie__img"
+                          />
+                        </Link>
+                      </figure>
+                      <div className="movie__description">
+                        <p className="movie__name">{movie.Title}</p>
+                        <p className="movie__date">{movie.Year}</p>
                       </div>
-                    ))
-                    .slice(0, 6)
-                )}
-              </div>
+                    </div>
+                  ))
+                  .slice(0, 8)
+              )}
             </div>
           </div>
-        </section>
-      </body>
+        </div>
+      </section>
     </>
   );
 };
